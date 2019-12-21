50df3b749990ddfcbdf72c315167225af4d5e194
Vlada Dusek <v.dusek96@gmail.com>
2019-12-21 22:35:46 +0100

    Provide get_command() method

--- dicewars/dicewars/ai/emm.py
+++ dicewars/dicewars/ai/emm.py
@@ -17,70 +17,101 @@ class AI:
         self.logger = logging.getLogger('AI')
 
     def ai_turn(self, board, nb_moves_this_turn, nb_turns_this_game, time_left):
-        EVAL_WEIGHT = 0.24
-        SUCC_WEIGHT = 0.60
-        HOLD_WEIGHT = 0.08
-        LOSE_WEIGHT = 0.08
+        return self.get_command(board)
 
-        PENALTY_EVAL_WEIGHT = 0.75
-        PENALTY_HOLD_WEIGHT = 0.25
+    def get_command(self, board):
 
-        ATTACK_TRESHOLD = random.uniform(0.07, 0.08)
+        ATTACK_HOLD_THRESHOLD = random.uniform(0.175, 0.2)
 
-        max_attack_coeff = -1000
+        ATTACK_WEIGHT = 0.85
+        HOLD_WEIGHT = 0.15
 
-        best_source = 0
-        best_target = 0
+        ATTACK_HOLD_WEIGHT = 0.5
+        DIFF_WIN_WEIGHT = 0.25
+        DIFF_LOSE_WEIGHT = 0.125
+        LOSE_HOLD_WEIGHT = 0.125
+
+        attack_hold_prob = []
+        diff_eval_win = []
+        diff_eval_lose = []
+        lose_hold_prob = []
+
+        current_eval = self.evaluate(board, self.player_name)
 
         for source, target in possible_attacks(board, self.player_name):
+
             win_board = copy.deepcopy(board)
-
-            win_board.areas[str(target.get_name())].set_owner(self.player_name)
-            win_board.areas[str(target.get_name())].set_dice(win_board.areas[str(source.get_name())].get_dice() - 1)
-            win_board.areas[str(source.get_name())].set_dice(1)
-
             lose_board = copy.deepcopy(board)
-            lose_board.areas[str(source.get_name())].set_dice(1)
 
-            successful_attack_probability = probability_of_successful_attack(
-                board, source.get_name(), target.get_name()
+            target_name_str = str(target.get_name())
+            target_name_int = target.get_name()
+            source_name_str = str(source.get_name())
+            source_name_int = source.get_name()
+
+            win_board.areas[target_name_str].set_owner(self.player_name)
+            win_board.areas[target_name_str].set_dice(win_board.areas[source_name_str].get_dice() - 1)
+            win_board.areas[source_name_str].set_dice(1)
+            lose_board.areas[source_name_str].set_dice(1)
+
+            # 1) attack_hold_prob
+
+            attack_prob = probability_of_successful_attack(board, source_name_int, target_name_int)
+            hold_prob = probability_of_holding_area(
+                board, target_name_int, win_board.areas[target_name_str].get_dice(), self.player_name
             )
-            holding_attack_area_probability = probability_of_holding_area(
-                board, target.get_name(), win_board.areas[str(target.get_name())].get_dice(), self.player_name
-            )
+            attack_hold_coeff = (ATTACK_WEIGHT * attack_prob + HOLD_WEIGHT * hold_prob) / 2
 
-            holding_source_probability = probability_of_holding_area(
-                board, source.get_name(), lose_board.areas[str(source.get_name())].get_dice(), self.player_name
-            )
+            if attack_hold_coeff > ATTACK_HOLD_THRESHOLD:
+                attack_hold_prob.append((source_name_int, target_name_int, attack_hold_coeff))
 
-            win_board_evaluation = self.evaluate(win_board, self.player_name)
-            lose_board_evaluation = self.evaluate(lose_board, self.player_name)
+                # 2) diff_eval_win
 
-            # more is better
-            lose_penalty = (PENALTY_EVAL_WEIGHT * lose_board_evaluation +
-                            PENALTY_HOLD_WEIGHT * holding_source_probability) / 2
+                win_eval = self.evaluate(win_board, self.player_name)
+                diff_eval_win_coeff = win_eval - current_eval
+                diff_eval_win.append((source_name_int, target_name_int, diff_eval_win_coeff))
 
-            # more is better
-            attack_coeff = (EVAL_WEIGHT * win_board_evaluation + SUCC_WEIGHT * successful_attack_probability +
-                            HOLD_WEIGHT * holding_attack_area_probability + LOSE_WEIGHT * lose_penalty) / 4
+                # 3) diff_eval_lose
 
-            self.logger.debug("source: " + str(source.get_dice()) + "   target: " + str(target.get_dice()) +
-                              "  attack_coeff = " + str(attack_coeff) + "   eval = " + str(win_board_evaluation) +
-                              "   attack_prob = " + str(successful_attack_probability) + "  hold_prob = " +
-                              str(holding_attack_area_probability) + "  lose = " + str(lose_penalty))
+                lose_eval = self.evaluate(lose_board, self.player_name)
+                diff_eval_lose_coeff = lose_eval - current_eval
+                diff_eval_lose.append((source_name_int, target_name_int, diff_eval_lose_coeff))
 
-            if attack_coeff > max_attack_coeff:
-                max_attack_coeff = attack_coeff
-                best_source = source
-                best_target = target
+                # 4) lose_hold_prob
 
-        if max_attack_coeff > ATTACK_TRESHOLD:
-            self.logger.debug("best_source = " + str(best_source.get_name()) + ", " + str(best_source.get_dice()))
-            self.logger.debug("best_target = " + str(best_target.get_name()) + ", " + str(best_target.get_dice()))
-            self.logger.debug("max_attack_coeff = " + str(max_attack_coeff))
-            return BattleCommand(best_source.get_name(), best_target.get_name())
+                lose_hold_prob_coeff = probability_of_holding_area(
+                    board, source_name_int, lose_board.areas[source_name_str].get_dice(), self.player_name
+                )
+                lose_hold_prob.append((source_name_int, target_name_int, lose_hold_prob_coeff))
 
-        return EndTurnCommand()
+                self.logger.debug("(" + str(source.get_dice()) + ", " + str(target.get_dice()) + ", " + str(attack_hold_coeff) +
+                                  ", " + str(diff_eval_win_coeff) + ", " + str(diff_eval_lose_coeff) + ", " +
+                                  str(lose_hold_prob_coeff) + ")")
+
+        # Sorting lists
+
+        attack_hold_prob.sort(key=lambda tup: tup[2])
+        diff_eval_win.sort(key=lambda tup: tup[2])
+        diff_eval_lose.sort(key=lambda tup: tup[2])
+        lose_hold_prob.sort(key=lambda tup: tup[2])
+
+        # List with possible actions
+
+        possible_actions = []
+
+        for source, target, attack_hold_coeff in attack_hold_prob:
+            diff_win_coeff = [tup for tup in diff_eval_win if tup[0] == source and tup[1] == target][0][2]
+            diff_lose_coeff = [tup for tup in diff_eval_lose if tup[0] == source and tup[1] == target][0][2]
+            lose_hold_coeff = [tup for tup in lose_hold_prob if tup[0] == source and tup[1] == target][0][2]
+            action_coeff = (ATTACK_HOLD_WEIGHT * attack_hold_coeff + DIFF_WIN_WEIGHT * diff_win_coeff +
+                            DIFF_LOSE_WEIGHT * diff_lose_coeff + LOSE_HOLD_WEIGHT * lose_hold_coeff) / 4
+            possible_actions.append((source, target, action_coeff))
+
+        possible_actions.sort(key=lambda tup: tup[2])
+
+        if possible_actions:
+            return BattleCommand(possible_actions[0][0], possible_actions[0][1])
+        else:
+            return EndTurnCommand()
 
     def evaluate(self, board, player_name):
         """ Evaluate the state of the game from the player perspective
@@ -103,6 +134,8 @@ class AI:
         enemies_score = sum(enemy["score"] for enemy in enemies.values())
         enemies_dices = sum(enemy["dices"] for enemy in enemies.values())
 
+        # me:    y = sqrt(x/n)
+        # enemy: y = 1 - sqrt(x/n)
         evaluation = (SCORE_WEIGHT * math.sqrt((player_score / max_score) / max_score) +
                       DICES_WEIGHT * math.sqrt((player_dices / total_dices) / 8 * max_score) +
                       SCORE_WEIGHT * (1 - math.sqrt((enemies_score / max_score) / max_score)) +
