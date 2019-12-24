666abd5f251934fc407afd6097129c53c1ee1e2e
Vlada Dusek <v.dusek96@gmail.com>
2019-12-24 08:34:33 +0100

    Add benchmark 23/12

--- README.md
+++ README.md
@@ -1,7 +1,52 @@
-### SUI
+## SUI
+
+
+### Benchmark 23. 12.
+
+#### 2 PLAYERS (100 games per run)
+
+- Win counts {'msv (AI)': 67, 'dt.sdc (AI)': 33}
+- Win counts {'msv (AI)': 68, 'dt.ste (AI)': 32}
+- Win counts {'msv (AI)': 72, 'dt.stei (AI)': 28}
+- Win counts {'dt.wpm_d (AI)': 33, 'msv (AI)': 67}
+- Win counts {'dt.wpm_s (AI)': 20, 'msv (AI)': 80}
+- Win counts {'dt.wpm_c (AI)': 40, 'msv (AI)': 60}
+
+#### 3 PLAYERS (150 games per run)
+
+- Win counts {'msv (AI)': 62, 'dt.ste (AI)': 54, 'dt.wpm_c (AI)': 34}
+- Win counts {'dt.wpm_d (AI)': 21, 'msv (AI)': 76, 'dt.ste (AI)': 53}
+- Win counts {'dt.wpm_s (AI)': 38, 'msv (AI)': 61, 'dt.ste (AI)': 51}
+- Win counts {'dt.stei (AI)': 40, 'msv (AI)': 74, 'dt.wpm_c (AI)': 36}
+- Win counts {'dt.stei (AI)': 50, 'dt.wpm_d (AI)': 34, 'msv (AI)': 66}
+- Win counts {'msv (AI)': 61, 'dt.stei (AI)': 55, 'dt.wpm_s (AI)': 34}
+- Win counts {'dt.wpm_d (AI)': 38, 'dt.wpm_c (AI)': 36, 'msv (AI)': 76}
+- Win counts {'dt.wpm_c (AI)': 45, 'dt.wpm_s (AI)': 25, 'msv (AI)': 80}
+- Win counts {'dt.wpm_s (AI)': 27, 'msv (AI)': 85, 'dt.wpm_d (AI)': 38}
+
+#### 4 PLAYERS (200 games per run)
+
+- Win counts {'dt.ste (AI)': 65, 'dt.stei (AI)': 49, 'msv (AI)': 51, 'dt.wpm_c (AI)': 35}
+- Win counts {'dt.wpm_d (AI)': 30, 'dt.ste (AI)': 52, 'msv (AI)': 55, 'dt.stei (AI)': 63}
+- Win counts {'dt.ste (AI)': 62, 'msv (AI)': 52, 'dt.wpm_s (AI)': 47, 'dt.stei (AI)': 39}
+- Win counts {'msv (AI)': 69, 'dt.stei (AI)': 47, 'dt.wpm_c (AI)': 45, 'dt.wpm_d (AI)': 39}
+- Win counts {'dt.wpm_c (AI)': 39, 'msv (AI)': 59, 'dt.wpm_s (AI)': 46, 'dt.stei (AI)': 56}
+- Win counts {'dt.wpm_d (AI)': 32, 'msv (AI)': 63, 'dt.stei (AI)': 71, 'dt.wpm_s (AI)': 34}
+- Win counts {'dt.wpm_d (AI)': 33, 'msv (AI)': 56, 'dt.wpm_s (AI)': 73, 'dt.wpm_c (AI)': 38}
+
+#### 5 PLAYERS (250 games per run)
+
+- Win counts {'dt.wpm_d (AI)': 23, 'dt.ste (AI)': 53, 'dt.stei (AI)': 84, 'dt.wpm_c (AI)': 29, 'msv (AI)': 61}
+- Win counts {'dt.wpm_s (AI)': 70, 'dt.wpm_d (AI)': 27, 'msv (AI)': 55, 'dt.stei (AI)': 77, 'dt.wpm_c (AI)': 21}
+- Win counts {'dt.wpm_d (AI)': 32, 'msv (AI)': 62, 'dt.wpm_s (AI)': 59, 'dt.ste (AI)': 69, 'dt.wpm_c (AI)': 28}
+- Win counts {'dt.wpm_d (AI)': 22, 'dt.stei (AI)': 63, 'dt.ste (AI)': 63, 'dt.wpm_s (AI)': 61, 'msv (AI)': 41}
+- Win counts {'msv (AI)': 48, 'dt.ste (AI)': 66, 'dt.wpm_s (AI)': 43, 'dt.stei (AI)': 70, 'dt.wpm_c (AI)': 23}
+
+
+### Benchmark 22. 12.
 
 #### 2 players:
-- ok
+- ...
 
 #### 3 players:
 - Win counts {'msv (AI)': 38, 'dt.ste (AI)': 34, 'dt.stei (AI)': 28}
@@ -24,5 +69,5 @@
 - Win counts {'msv (AI)': 46, 'dt.wpm_s (AI)': 24, 'dt.stei (AI)': 54, 'dt.wpm_d (AI)': 26}
 - Win counts {'dt.wpm_s (AI)': 59, 'dt.wpm_c (AI)': 30, 'dt.wpm_d (AI)': 22, 'msv (AI)': 39}
 
-### 5 PLAYERS ###
+### 5 PLAYERS
 - ...
--- dicewars/run.sh
+++ dicewars/run.sh
@@ -4,54 +4,68 @@
 
 touch out.txt
 
-echo "Bots: msv, ste, stei, wpm_c, wpm_d, wpm_s" >> out.txt
+echo "Bots: msv, sdc, ste, stei, wpm_c, wpm_d, wpm_s" >> out.txt
 
-echo "### 3 PLAYERS ###" >> out.txt
-./scripts/dicewars-ai-only.py --debug --logdir logs/ --ai msv dt.ste dt.stei --nb-games 100 >> out.txt
+echo "### 2 PLAYERS (100 games per run) ###" >> out.txt
+./scripts/dicewars-ai-only.py --debug --logdir logs/ --ai msv dt.sdc --nb-games 100 >> out.txt
 echo >> out.txt
-./scripts/dicewars-ai-only.py --debug --logdir logs/ --ai msv dt.ste dt.wpm_c --nb-games 100 >> out.txt
+./scripts/dicewars-ai-only.py --debug --logdir logs/ --ai msv dt.ste --nb-games 100 >> out.txt
 echo >> out.txt
-./scripts/dicewars-ai-only.py --debug --logdir logs/ --ai msv dt.ste dt.wpm_d --nb-games 100 >> out.txt
+./scripts/dicewars-ai-only.py --debug --logdir logs/ --ai msv dt.stei --nb-games 100 >> out.txt
 echo >> out.txt
-./scripts/dicewars-ai-only.py --debug --logdir logs/ --ai msv dt.ste dt.wpm_s --nb-games 100 >> out.txt
+./scripts/dicewars-ai-only.py --debug --logdir logs/ --ai msv dt.wpm_d --nb-games 100 >> out.txt
 echo >> out.txt
-./scripts/dicewars-ai-only.py --debug --logdir logs/ --ai msv dt.stei dt.wpm_c --nb-games 100 >> out.txt
+./scripts/dicewars-ai-only.py --debug --logdir logs/ --ai msv dt.wpm_s --nb-games 100 >> out.txt
 echo >> out.txt
-./scripts/dicewars-ai-only.py --debug --logdir logs/ --ai msv dt.stei dt.wpm_d --nb-games 100 >> out.txt
-echo >> out.txt
-./scripts/dicewars-ai-only.py --debug --logdir logs/ --ai msv dt.stei dt.wpm_s --nb-games 100 >> out.txt
-echo >> out.txt
-./scripts/dicewars-ai-only.py --debug --logdir logs/ --ai msv dt.wpm_c dt.wpm_d --nb-games 100 >> out.txt
-echo >> out.txt
-./scripts/dicewars-ai-only.py --debug --logdir logs/ --ai msv dt.wpm_c dt.wpm_s --nb-games 100 >> out.txt
-echo >> out.txt
-./scripts/dicewars-ai-only.py --debug --logdir logs/ --ai msv dt.wpm_d dt.wpm_s --nb-games 100 >> out.txt
+./scripts/dicewars-ai-only.py --debug --logdir logs/ --ai msv dt.wpm_c --nb-games 100 >> out.txt
 echo >> out.txt
 
-echo "### 4 PLAYERS ###" >> out.txt
-./scripts/dicewars-ai-only.py --debug --logdir logs/ --ai msv dt.ste dt.stei dt.wpm_c --nb-games 150 >> out.txt
+echo "### 3 PLAYERS (150 games per run) ###" >> out.txt
+./scripts/dicewars-ai-only.py --debug --logdir logs/ --ai msv dt.ste dt.stei --nb-games 150 >> out.txt
 echo >> out.txt
-./scripts/dicewars-ai-only.py --debug --logdir logs/ --ai msv dt.ste dt.stei dt.wpm_d --nb-games 150 >> out.txt
+./scripts/dicewars-ai-only.py --debug --logdir logs/ --ai msv dt.ste dt.wpm_c --nb-games 150 >> out.txt
 echo >> out.txt
-./scripts/dicewars-ai-only.py --debug --logdir logs/ --ai msv dt.ste dt.stei dt.wpm_s --nb-games 150 >> out.txt
+./scripts/dicewars-ai-only.py --debug --logdir logs/ --ai msv dt.ste dt.wpm_d --nb-games 150 >> out.txt
 echo >> out.txt
-./scripts/dicewars-ai-only.py --debug --logdir logs/ --ai msv dt.stei dt.wpm_c dt.wpm_d --nb-games 150 >> out.txt
+./scripts/dicewars-ai-only.py --debug --logdir logs/ --ai msv dt.ste dt.wpm_s --nb-games 150 >> out.txt
 echo >> out.txt
-./scripts/dicewars-ai-only.py --debug --logdir logs/ --ai msv dt.stei dt.wpm_c dt.wpm_s --nb-games 150 >> out.txt
+./scripts/dicewars-ai-only.py --debug --logdir logs/ --ai msv dt.stei dt.wpm_c --nb-games 150 >> out.txt
 echo >> out.txt
-./scripts/dicewars-ai-only.py --debug --logdir logs/ --ai msv dt.stei dt.wpm_d dt.wpm_s --nb-games 150 >> out.txt
+./scripts/dicewars-ai-only.py --debug --logdir logs/ --ai msv dt.stei dt.wpm_d --nb-games 150 >> out.txt
 echo >> out.txt
-./scripts/dicewars-ai-only.py --debug --logdir logs/ --ai msv dt.wpm_c dt.wpm_d dt.wpm_s --nb-games 150 >> out.txt
+./scripts/dicewars-ai-only.py --debug --logdir logs/ --ai msv dt.stei dt.wpm_s --nb-games 150 >> out.txt
+echo >> out.txt
+./scripts/dicewars-ai-only.py --debug --logdir logs/ --ai msv dt.wpm_c dt.wpm_d --nb-games 150 >> out.txt
+echo >> out.txt
+./scripts/dicewars-ai-only.py --debug --logdir logs/ --ai msv dt.wpm_c dt.wpm_s --nb-games 150 >> out.txt
+echo >> out.txt
+./scripts/dicewars-ai-only.py --debug --logdir logs/ --ai msv dt.wpm_d dt.wpm_s --nb-games 150 >> out.txt
 echo >> out.txt
 
-echo "### 5 PLAYERS ###" >> out.txt
-./scripts/dicewars-ai-only.py --debug --logdir logs/ --ai msv dt.ste dt.stei dt.wpm_c dt.wpm_d --nb-games 200 >> out.txt
+echo "### 4 PLAYERS (200 games per run) ###" >> out.txt
+./scripts/dicewars-ai-only.py --debug --logdir logs/ --ai msv dt.ste dt.stei dt.wpm_c --nb-games 200 >> out.txt
 echo >> out.txt
-./scripts/dicewars-ai-only.py --debug --logdir logs/ --ai msv dt.wpm_s dt.stei dt.wpm_c dt.wpm_d --nb-games 200 >> out.txt
+./scripts/dicewars-ai-only.py --debug --logdir logs/ --ai msv dt.ste dt.stei dt.wpm_d --nb-games 200 >> out.txt
 echo >> out.txt
-./scripts/dicewars-ai-only.py --debug --logdir logs/ --ai msv dt.ste dt.wpm_s dt.wpm_c dt.wpm_d --nb-games 200 >> out.txt
+./scripts/dicewars-ai-only.py --debug --logdir logs/ --ai msv dt.ste dt.stei dt.wpm_s --nb-games 200 >> out.txt
 echo >> out.txt
-./scripts/dicewars-ai-only.py --debug --logdir logs/ --ai msv dt.ste dt.stei dt.wpm_s dt.wpm_d --nb-games 200 >> out.txt
+./scripts/dicewars-ai-only.py --debug --logdir logs/ --ai msv dt.stei dt.wpm_c dt.wpm_d --nb-games 200 >> out.txt
 echo >> out.txt
-./scripts/dicewars-ai-only.py --debug --logdir logs/ --ai msv dt.ste dt.stei dt.wpm_c dt.wpm_s --nb-games 200 >> out.txt
+./scripts/dicewars-ai-only.py --debug --logdir logs/ --ai msv dt.stei dt.wpm_c dt.wpm_s --nb-games 200 >> out.txt
+echo >> out.txt
+./scripts/dicewars-ai-only.py --debug --logdir logs/ --ai msv dt.stei dt.wpm_d dt.wpm_s --nb-games 200 >> out.txt
+echo >> out.txt
+./scripts/dicewars-ai-only.py --debug --logdir logs/ --ai msv dt.wpm_c dt.wpm_d dt.wpm_s --nb-games 200 >> out.txt
+echo >> out.txt
+
+echo "### 5 PLAYERS (250 games per run) ###" >> out.txt
+./scripts/dicewars-ai-only.py --debug --logdir logs/ --ai msv dt.ste dt.stei dt.wpm_c dt.wpm_d --nb-games 250 >> out.txt
+echo >> out.txt
+./scripts/dicewars-ai-only.py --debug --logdir logs/ --ai msv dt.wpm_s dt.stei dt.wpm_c dt.wpm_d --nb-games 250 >> out.txt
+echo >> out.txt
+./scripts/dicewars-ai-only.py --debug --logdir logs/ --ai msv dt.ste dt.wpm_s dt.wpm_c dt.wpm_d --nb-games 250 >> out.txt
+echo >> out.txt
+./scripts/dicewars-ai-only.py --debug --logdir logs/ --ai msv dt.ste dt.stei dt.wpm_s dt.wpm_d --nb-games 250 >> out.txt
+echo >> out.txt
+./scripts/dicewars-ai-only.py --debug --logdir logs/ --ai msv dt.ste dt.stei dt.wpm_c dt.wpm_s --nb-games 250 >> out.txt
 echo >> out.txt
